bb4c17f99d9b819b53daac4219debf3e78caab51
Chad Levenhagen <Chad.Levenhagen@polkcountyiowa.gov>
2022-06-22 21:25:00 -0500

    Added struct changes

---
+++ StructChanges/Example.cs
@@ -0,0 +1,14 @@
+﻿namespace StructChanges;
+
+struct Person
+{
+    public string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string LastName { get; set; }
+
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
