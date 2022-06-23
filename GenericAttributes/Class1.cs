6d4314a60966477fb8efadeaae767f0782affca4
Chad Levenhagen <Chad.Levenhagen@polkcountyiowa.gov>
2022-06-22 19:42:45 -0500

    Added generic attributes

---
+++ GenericAttributes/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericAttributes;
+
+public class TypeAttribute : Attribute
+{
+    public TypeAttribute(Type t) => ParamType = t;
+
+    public Type ParamType { get; }
+}
+
+public class TypeAttributeUsage
+{
+    [TypeAttribute(typeof(string))]
+    public static string? Method() => default;
+}
+
+// This again? I thought this was in C#10. Nope, it never made it. But they promised us that it would maybe be in C#11 maybe. Potentially.
+
+#region C#11 Preview, need <LangVersion>Preview</LangVersion> in csproj
+
+public class GenericAttribute<T> : Attribute { }
+
+public class GenericAttributeUsage
+{
+    [GenericAttribute<string>()]
+    public static string? Method() => default;
+}
+
+#endregion
