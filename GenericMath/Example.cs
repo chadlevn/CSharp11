8c20a3ef850933afa6cd6885bb8f92cb069e8be2
Chad Levenhagen <Chad.Levenhagen@polkcountyiowa.gov>
2022-06-22 21:17:12 -0500

    Added Generiic Math

---
+++ GenericMath/Example.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace GenericMath;
+
+public class Example
+{
+    public static int Sum(int[] valuesToSum)
+    {
+        int result = 0;
+        foreach (var value in valuesToSum)
+            result += value;
+        return result;
+    }
+
+    #region Generic Maths
+
+    public static T Sum<T>(T[] valuesToSum) where T : INumber<T>
+    {
+        T result = T.AdditiveIdentity;
+        foreach (var value in valuesToSum)
+            result += value;
+        return result;
+    }
+
+    #endregion
+}
