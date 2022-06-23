dd602a2872c265c69b8a45f9e62be2a509c423e7
Chad Levenhagen <Chad.Levenhagen@polkcountyiowa.gov>
2022-06-22 21:31:12 -0500

    Added more examples

--- RequiredKeyword/Example.cs
+++ RequiredKeyword/Example.cs
@@ -1,11 +1,16 @@
 ﻿namespace RequiredKeyword;
 
+/// <summary>
+///     The new required keyword can replace the use of constructors.
+/// </summary>
 public class Person
 {
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public int Age { get; set; }
 }
+
+#region How It Works
 
 public class PersonContainer
 {
@@ -21,3 +26,30 @@ public class PersonContainer
         };
     }
 }
+
+#endregion
+
+#region Extending a Class
+
+// Yes, developers are people too.
+public class Developer : Person
+{
+    // Don't have to worry about a bunch of different constructors here!
+}
+
+public class DeveloperContainer
+{
+    private readonly Developer _person;
+
+    public DeveloperContainer()
+    {
+        // The below code won't compile unless all required fields are passed along. This helps to eliminate the need for lengthy constructors.
+        _person = new Developer()
+        {
+            FirstName = "John",
+            LastName = "Smith"
+        };
+    }
+}
+
+#endregion
--- StructChanges/Example.cs
+++ StructChanges/Example.cs
@@ -6,6 +6,11 @@ struct Person
     public string? MiddleName { get; set; }
     public string LastName { get; set; }
 
+    // The C# 11 compiler ensures that all fields of a struct type are initialized to their default value as part of executing a constructor.
+    // This change means any field or auto property not initialized by a constructor is automatically initialized by the compiler.
+    // Structs where the constructor doesn't definitely assign all fields now compile, and any fields not explicitly initialized are set to their default value.
+    // You can read more about how this change affects struct initialization in the article on structs.
+
     public Person(string firstName, string lastName)
     {
         FirstName = firstName;
